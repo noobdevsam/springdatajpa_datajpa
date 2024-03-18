7fb9bb94736c72e0b1a953495d0147777d90fa00
mrdevsam <mdshoykotislam05@gmail.com>
2024-03-18 23:10:39 +0600

    first commit

---
+++ src/main/java/com/example/springdatajpa_datajpa/SpringdatajpaDatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpa_datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaDatajpaApplication.class, args);
+	}
+
+}
