44bb4db641ef77eab285c1ee8ee718d96ebb2aa9
mrdevsam <mdshoykotislam05@gmail.com>
2024-03-29 19:42:43 +0600

    overriding jpa default null handling

--- src/main/java/com/example/springdatajpa_datajpa/repos/BookRepo.java
+++ src/main/java/com/example/springdatajpa_datajpa/repos/BookRepo.java
@@ -3,11 +3,14 @@ package com.example.springdatajpa_datajpa.repos;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 
 import com.example.springdatajpa_datajpa.model.Book;
 
 public interface BookRepo extends JpaRepository<Book, Long>{
     Optional<Book> findBookByTitle(String title);
     Book readByTitle(String title);
-    Book getByTitle(String title);
+
+    @Nullable
+    Book getByTitle(@Nullable String title);
 }
